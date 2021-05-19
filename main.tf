506eb65d6d1d6e9c8679c30f9ab1cf5a2bbe7a45
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-05-18 22:52:04 -0400

    update  tf

--- main.tf
+++ main.tf
@@ -15,39 +15,15 @@ terraform {
     organization = "zest-tech"
 
     workspaces {
-      name = "gh-actions-demo"
+      prefix = "base-infra"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
-resource "random_pet" "sg" {}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World1" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+esource "aws_ecr_repository" "test_ecr_repo" {
+  name = "test-ecr-repo"
 }
