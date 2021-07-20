6d469c53ff22263284f8f5d6bfc72db63e43bc44
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-07-19 21:39:45 -0400

    first steps

--- frontend/cloudfront.tf
+++ frontend/cloudfront.tf
@@ -182,24 +182,6 @@ module "log_bucket" {
 # Route53
 ##########
 
-module "records" {
-  source  = "terraform-aws-modules/route53/aws//modules/records"
-  version = "~> 2.0"
-
-  zone_name = data.aws_route53_zone.this.name
-
-  records = [
-    {
-      name = local.subdomain
-      type = "A"
-      alias = {
-        name    = module.cloudfront.cloudfront_distribution_domain_name
-        zone_id = module.cloudfront.cloudfront_distribution_hosted_zone_id
-
-      }
-    },
-  ]
-}
 
 ###########################
 # Origin Access Identities
