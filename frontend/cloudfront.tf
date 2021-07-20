682e38a66e65a45a015a1b1391b1fa261c35d74f
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-07-19 21:16:13 -0400

    first steps

--- frontend/cloudfront.tf
+++ frontend/cloudfront.tf
@@ -134,10 +134,9 @@ module "acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "~> 3.0"
 
-  domain_name = local.domain_name
+  domain_name = "${local.subdomain}.${local.domain_name}"
   zone_id     = data.aws_route53_zone.this.id
-  subject_alternative_names = [
-  "${local.subdomain}.${local.domain_name}"]
+
 }
 
 #############
