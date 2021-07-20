925092de8522eda71215db12b5b18735bac82161
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-07-19 21:36:42 -0400

    first steps

--- frontend/cloudfront.tf
+++ frontend/cloudfront.tf
@@ -186,7 +186,6 @@ module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
 
-  zone_id   = data.aws_route53_zone.this.zone_id
   zone_name = data.aws_route53_zone.this.name
 
   records = [
