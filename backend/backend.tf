1a31f0e7e33620175732b622641db1e4437d59ce
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-07-21 20:06:04 -0400

    add flow logs

--- backend/backend.tf
+++ backend/backend.tf
@@ -12,6 +12,11 @@ module "vpc" {
   enable_nat_gateway     = true
   single_nat_gateway     = false
   one_nat_gateway_per_az = true
+
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+  flow_log_max_aggregation_interval    = 60
 }
 
 ###################
