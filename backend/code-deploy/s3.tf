2cbe819c67c37e5a7e72c0231e33c200c32180f9
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-07-23 12:19:46 -0400

    add code-deploy

--- backend/code-deploy/s3.tf
+++ backend/code-deploy/s3.tf
@@ -1,5 +1,5 @@
 # create an S3 bucket
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "code-deploy-bucket" {
   bucket = "Xeniapp-backend-codedeploy-${var.env_name}"
   acl    = "private"
 }
