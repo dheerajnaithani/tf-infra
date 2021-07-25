97e5b558161ba7b9a922b2893c832776ea8e914a
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-07-25 12:21:27 -0400

    add cloudfront

--- frontend/cloudfront.tf
+++ frontend/cloudfront.tf
@@ -185,25 +185,7 @@ module "log_bucket" {
 # Route53
 ##########
 
-module "records" {
-  source  = "terraform-aws-modules/route53/aws//modules/records"
-  version = "~> 2.0"
 
-  zone_id = data.aws_route53_zone.this.zone_id
-
-  records = [
-    for dns in local.all_dns :
-    {
-      name = dns
-      type = "A"
-      alias = {
-        name    = module.cloudfront.cloudfront_distribution_domain_name
-        zone_id = module.cloudfront.cloudfront_distribution_hosted_zone_id
-
-      }
-    }
-  ]
-}
 
 ###########################
 # Origin Access Identities
