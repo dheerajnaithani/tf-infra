dcbbf3cb28811aeced680ed8bfef02b538415598
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-07-25 00:04:16 -0400

    Add sg ingress rule for ssl

--- backend/backend.tf
+++ backend/backend.tf
@@ -288,7 +288,9 @@ module "alb_security_group" {
   ingress_cidr_blocks = [
   "0.0.0.0/0"]
   ingress_rules = [
-  "http-80-tcp"]
+    "http-80-tcp",
+    "https-443-tcp"
+  ]
 
   egress_rules = [
   "all-all"]
