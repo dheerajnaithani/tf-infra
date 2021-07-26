f43c6622ed86f17e016884a5aef02b72d4d4a04f
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-07-26 14:58:05 -0400

    add mongodb conf

--- main.tf
+++ main.tf
@@ -5,11 +5,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.50.0"
+      version = "3.51.0"
     }
     random = {
       source  = "hashicorp/random"
       version = "3.1.0"
+    }
+    mongodbatlas = {
+      source  = "terraform-providers/mongodbatlas"
+      version = "0.9.1"
     }
   }
   required_version = "~> 1.0.2"
